599dca2c3fdf6f235f59251e34a2e14a90ca790a
DianaRangelova <diana_kirilova@mail.bg>
2024-02-09 02:40:00 +0200

    Methods in C#

---
+++ P04-Calculate-Rectangle-Area/Program.cs
@@ -0,0 +1,10 @@
+﻿int lenght = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+
+Console.WriteLine(CalculateRectangleArea(lenght, width));
+
+static int CalculateRectangleArea(int l, int w)
+{
+    int area = l * w;
+    return area;
+}
---
+++ P06-Math-Power/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = int.Parse(Console.ReadLine());
+int power = int.Parse(Console.ReadLine());
+
+Console.WriteLine(MathPow(number, power));
+
+static int MathPow(int baseNumber, int powerNumber)
+{
+    int output = baseNumber;
+    for (int i = 2; i <= powerNumber; i++)
+    {
+        output *= baseNumber;
+    }
+    return output;
+}
